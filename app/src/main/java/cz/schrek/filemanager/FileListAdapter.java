be3497f199b17e50c7f547ab6287a425b3ba5d4e
Ondřej Schrek <ondrej.schrek@gmail.com>
2016-08-09 18:15:50 +0200

    uprava designu + pridany detaily k polozce listu

--- app/src/main/java/cz/schrek/filemanager/FileListAdapter.java
+++ app/src/main/java/cz/schrek/filemanager/FileListAdapter.java
@@ -1,44 +1,90 @@
 package cz.schrek.filemanager;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
 
 import java.io.File;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.function.DoublePredicate;
 
 /**
  * Created by ondra on 7. 8. 2016.
  */
 public class FileListAdapter extends ArrayAdapter<File> {
     private File[] fileContent;
+    private Context context;
 
     public FileListAdapter(Context context, File[] files) {
         super(context, R.layout.row, R.id.label, files);
         fileContent = files;
-    }
+        this.context = context;
 
-    public void setFileContent(File[] fileContent) {
-        this.fileContent = fileContent;
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
         View row = super.getView(position, convertView, parent);
         ImageView icon = (ImageView) row.findViewById(R.id.icon);
         TextView label = (TextView) row.findViewById(R.id.label);
+        TextView details = (TextView) row.findViewById(R.id.details);
 
-        if (fileContent[position].isDirectory()) {
-            icon.setImageResource(R.drawable.ic_folder_black_24dp);
-        } else if (fileContent[position].isFile()) {
+        File selected = fileContent[position];
+
+        SimpleDateFormat formater = new SimpleDateFormat("dd.MM. yy HH:mm");
+        Date date = new Date(selected.lastModified());
+
+        if (selected.isDirectory()) {
+            if (selected.list() == null) {
+                icon.setImageResource(R.drawable.ic_folder_open_black_24dp);
+                int color = Color.parseColor("#808080"); //The color u want
+                icon.setColorFilter(color);
+            } else {
+                icon.setImageResource(R.drawable.ic_folder_black_24dp);
+                int color = Color.parseColor("#ffc425"); //The color u want
+                icon.setColorFilter(color);
+            }
+            details.setText("<dir> \t " + formater.format(date));
+        } else if (selected.isFile()) {
             icon.setImageResource(R.drawable.ic_insert_drive_file_black_24dp);
+            int color = Color.parseColor("#004d99"); //The color u want
+            icon.setColorFilter(color);
+
+            String fileSize = getFileSize(selected.length());
+            details.setText(fileSize + " \t " + formater.format(date));
         }
 
-        String fileName = label.getText().toString();
-        label.setText(fileName.substring(1));
+        label.setText(selected.getName());
 
         return row;
     }
 
+    private String getFileSize(double length) {
+        int rank = 0;
+        while (length >= 1024) {
+            length /= 1024;
+            rank++;
+        }
+
+        DecimalFormat decForm = new DecimalFormat(".#");
+
+        switch (rank) {
+            case 0:
+                return decForm.format(length) + " B";
+            case 1:
+                return decForm.format(length) + " kB";
+            case 2:
+                return decForm.format(length) + " MB";
+            default:
+                return decForm.format(length) + " GB";
+        }
+    }
 
 
 }
--- app/src/main/java/cz/schrek/filemanager/MainActivity.java
+++ app/src/main/java/cz/schrek/filemanager/MainActivity.java
@@ -1,13 +1,15 @@
 package cz.schrek.filemanager;
 
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Environment;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.*;
 
 import java.io.File;
 import java.io.FileFilter;
@@ -17,8 +19,11 @@ import java.util.Comparator;
 public class MainActivity extends AppCompatActivity {
     private File rootFile;
     private File[] fileContent;
+    boolean doubleBackToExitPressedOnce = false;
+
 
     private TextView path;
+    private TextView isEmpty;
     private ListView fileList;
 
     @Override
@@ -73,10 +78,19 @@ public class MainActivity extends AppCompatActivity {
             numFiles = files.length;
         }
         fileContent = new File[numDirs + numFiles];
-        if (dirs != null)
+
+        if (numDirs + numFiles == 0) {
+            isEmpty.setVisibility(View.VISIBLE);
+        } else {
+            isEmpty.setVisibility(View.GONE);
+        }
+
+        if (dirs != null) {
             System.arraycopy(dirs, 0, fileContent, 0, dirs.length);
-        if (files != null)
+        }
+        if (files != null) {
             System.arraycopy(files, 0, fileContent, dirs.length, files.length);
+        }
 
         Log.wtf("array print", Arrays.toString(fileContent));
 
@@ -86,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         path = (TextView) findViewById(R.id.path);
+        isEmpty = (TextView) findViewById(R.id.isEmpty);
         fileList = (ListView) findViewById(R.id.fileList);
         rootFile = Environment.getRootDirectory().getParentFile();
 
@@ -96,6 +111,8 @@ public class MainActivity extends AppCompatActivity {
                 if (selected.isDirectory()) {
                     rootFile = selected;
                     getFileContent();
+                } else if (selected.isFile()) {
+                    //TODO dodelat otevreni defaultni aplikaci
                 }
             }
         });
@@ -105,11 +122,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         File parent = rootFile.getParentFile();
-        if(parent != null){
+        if (parent != null) {
             rootFile = parent;
             getFileContent();
-        }else{
-            finish();
+        } else {
+            if (doubleBackToExitPressedOnce) {
+                super.onBackPressed();
+                return;
+            }
+
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Pro ukonceni aplikace stisknete zpet.", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 2000);
         }
     }
 
